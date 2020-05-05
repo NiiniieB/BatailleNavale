aa1ad9a9acde77e56636179d0e49e7cd58fc673e
Niiniie <Melanie_Betourne_student2019@wilder.school>
2020-05-05 16:13:59 +0200

    Commit fin de cours

--- src/main/java/bateau/Bateau.java
+++ src/main/java/bateau/Bateau.java
@@ -1,4 +1,14 @@
 package bateau;
 
+import coordonnees.Coordonnees;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bateau {
+    private List<Coordonnees> coordonnees = new ArrayList<>();
+    private  int size;
+
+    public Bateau ( ) {
+    }
 }
